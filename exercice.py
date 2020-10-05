818e06090ece1e5829afc52b35acd6f5581cf6d8
EdouardFrechette <71038294+edouard20@users.noreply.github.com>
2020-10-05 14:39:48 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 
 from random import randint
 from structs import Queue, Stack
-
+from collections import deque
 
 def reverse_data(data: list = None):
     # TODO: Demander 10 valeurs à l'utilisateur,
@@ -11,20 +11,29 @@ def reverse_data(data: list = None):
     # et les retourner en ordre inverse, sans utiliser de liste.
 
     if data is None:
-        pass  # Demander les valeurs ici
+       data = [input("Entrez une valeur: ") for _ in range(10)] 
+      # Stocker le résultat ici
+    s = Stack()
+    s.put_many(data)
 
-    reversed_data = None  # Stocker le résultat ici
-
-    return reversed_data
+    return [s.get() for _ in range(len(s))]
 
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
-    return Stack()
+    temp_stack = Stack()
+    for i in range(len(data) - position - 1):
+        temp_stack.get(data.put(i))
+    data.put(position)
+    for i in range(len(data)):
+        data.get(temp_stack.put(i))
+
+    return data
 
 
 def delete_nth_from_queue(data: Queue, position: int) -> Queue:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
+
     return Queue()
 
 
@@ -49,29 +58,29 @@ def string_and_structs(string: str) -> tuple:
 
 
 def main() -> None:
-    print("On inverse des données...")
-    print(f"Résultat: {reverse_data()}")
+    # print("On inverse des données...")
+    # print(f"Résultat: {reverse_data()}")
 
     n = 4
     lifo = Stack()
     lifo.put_many([i for i in range(20)])
     print(f"On retire l'élément à la position {n} de la pile et on obtient: {delete_nth_from_stack(lifo, n)}")
 
-    n = 6
-    fifo = Queue()
-    fifo.put_many([i for i in range(20)])
-    print(f"On retire l'élément à la position {n} de la file et on obtient: {delete_nth_from_queue(fifo, n)}")
+    # n = 6
+    # fifo = Queue()
+    # fifo.put_many([i for i in range(20)])
+    # print(f"On retire l'élément à la position {n} de la file et on obtient: {delete_nth_from_queue(fifo, n)}")
 
-    lifo = Stack()
-    lifo.put_many([randint(0, 1000) for _ in range(20)])
-    print(f"On ordonne une file: {sort_queue(lifo)}")
+    # lifo = Stack()
+    # lifo.put_many([randint(0, 1000) for _ in range(20)])
+    # print(f"On ordonne une file: {sort_stack(lifo)}")
 
-    fifo = Queue()
-    fifo.put_many([randint(0, 1000) for _ in range(20)])
-    print(f"On ordonne une file: {sort_queue(fifo)}")
+    # fifo = Queue()
+    # fifo.put_many([randint(0, 1000) for _ in range(20)])
+    # print(f"On ordonne une file: {sort_queue(fifo)}")
 
-    sequence = "te!eYy.E6e/T"
-    print(f"Le résulat de la manipulation de la séquence: {string_and_structs(sequence)}")
+    # sequence = "te!eYy.E6e/T"
+    # print(f"Le résulat de la manipulation de la séquence: {string_and_structs(sequence)}")
 
 
 if __name__ == '__main__':
